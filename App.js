f0781ea3be10786c5a68003a32c9bbd14f0cfd81
Samin Thapa Kshetry <136580949+saminthapa@users.noreply.github.com>
2024-01-17 11:33:31 +0545

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import './style.css'
 function App(){
         const [number, setNumber] = useState(0)
     return <div>
-        <h1>Simple Counter Tool</h1><hr></hr><hr></hr>
+        <div className="hadder">
+        <h2>Simple Counter Tool</h2>
+        </div>
+        
         <h1> {number}</h1> 
         <button onClick={()=>setNumber(number+1) } className="button">Add</button>
         <button onClick={()=>setNumber(number-1) } className="button">Less</button>
-        <button onClick={()=>setNumber(0) } className="button">Reset</button>
+        <button onClick={()=>setNumber(0) } className="button-r">Reset</button>
+        <h3>NOTE:- This is a CounterApp project on <span>React.js</span> using <span>useState Hook</span>.</h3>
     </div>
 }
 
